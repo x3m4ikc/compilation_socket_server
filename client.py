15d79e55236d9206735bbbb1ecbecb859ebd5011
Timur <vt@greentechs.pro>
2023-07-10 17:00:01 +0300

    Update client.py server.py

--- client.py
+++ client.py
@@ -1,49 +1,55 @@
+import os
 import socket
-import argparse
+
+SIZE = 1024
+FORMAT = "utf"
 
 
-def send_files(host, port, files, makefile=None):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        num_files = len(files)
-        header = 'Number: {}\n'.format(num_files)
-        if makefile is not None:
-            header += 'Makefile: {}\n'.format(makefile)
-        s.sendall(header.encode())
-        for file in files:
-            with open(file, 'rb') as f:
-                filename = 'File {}\n'.format(file)
-                s.sendall(filename.encode() + f.read())
+class Client:
+    def __init__(self, ip="127.0.0.1", port=1235):
+        self.ip = ip
+        self.port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.client_socket.connect((self.ip, self.port))
+
+    def handle_command(self):
+        command = input()
+        self.client_socket.send(command.encode(FORMAT))
+        command, filename = command.split()
+
+        if command == "File":
+            self.send_file(filename)
+            self.get_result()
+
+        if command == "Number":
+            for _ in range(int(filename)):
+                self.handle_command()
+
+#        if command == "Upgrade":
+#            self.send_file(filename)
+#            self.get_result()
+
+    def send_file(self, filename):
+        file = open(f"data/{filename}", "r")
+        data = file.read()
+        self.client_socket.send(filename.encode(FORMAT))
+        message = self.client_socket.recv(SIZE).decode(FORMAT)
+        print(f"Server`s answer: {message}")
+        self.client_socket.send(data.encode(FORMAT))
+        file.close()
+
+    def get_result(self):
+        result = self.client_socket.recv(SIZE).decode(FORMAT)
+        print(f"Result of file compilation: {result}")
+
+    def close_connection(self):
+        self.client_socket.close()
 
 
-def upgrade_server(host, port, files, makefile=None):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        header = 'Upgrade\n'
-        num_files = len(files)
-        header += 'Number: {}\n'.format(num_files)
-        if makefile is not None:
-            header += 'Makefile: {}\n'.format(makefile)
-        s.sendall(header.encode())
-        for file in files:
-            with open(file, 'rb') as f:
-                filename = 'File {}\n'.format(file)
-                s.sendall(filename.encode() + f.read())
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('host', nargs='?', default='127.0.0.1')
-    parser.add_argument('port', nargs='?', type=int, default=1234)
-    parser.add_argument('files', nargs='+')
-    parser.add_argument('-m', '--makefile')
-    parser.add_argument('-u', '--upgrade', action='store_true')
-    args = parser.parse_args()
-    if args.upgrade:
-        upgrade_server(args.host, args.port, args.files, args.makefile)
-    else:
-        send_files(args.host, args.port, args.files, args.makefile)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    client = Client()
+    client.connect()
+    client.handle_command()
+    client.close_connection()
